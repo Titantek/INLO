64ca1eae7eb6fb3959024c051d719be19247eb4d
Jan Steiner <jan.steiner@he-arc.ch>
2023-03-21 17:20:45 +0100

    update for sonar

--- src/wallet.py
+++ src/wallet.py
@@ -13,17 +13,16 @@ class Wallet(object):
     def __init__(self, initial_amount=0):
         self.balance = initial_amount
 
-
-    def add_cash(self, amount):
-        """Adds the specified amount of money to the current Wallet object"""
-        self.balance += amount
-
-    def spend_money(self, amount):
+    def spend_cash(self, amount):
         """Removes the specified amount of money from the current Wallet object"""
         if self.balance < amount:
             raise InsufficientAmount(f"Not enough available to spend {amount}")
 
         self.balance -= amount
-    
+
+    def add_cash(self, amount):
+        """Adds the specified amount of money to the current Wallet object"""
+        self.balance += amount
+
     def get_balance(self):
         return self.balance
